673a94e62368b43e39aa0ad2129f4d265e0a0213
actionmen71 <37360288+actionmen71@users.noreply.github.com>
2021-10-20 14:13:44 +0530

    Add files via upload

--- Harrytut/tcs program 4.py
+++ Harrytut/tcs program 4.py
@@ -1,9 +1,38 @@
-number=int(input("enter number:"))
+def getSum(n):
+    s = 0
 
-if (number <200 and number >10000):
+    while (n != 0):
+        e = (n % 10)
+        s = s + (e ** 3)
+        n = n // 10
+
+    return s
+
+
+number = int(input("enter number:"))
+
+if (number < 200 or number > 10000):
     print("Invalid input! Try again")
 else:
-    for i in range(200,number):
+    for i in range(200, number+1):
+        number2=number
+
+
+        num = int(getSum(i))
+
+        if(number2 == num):
+            print("Your output is:", num)
+            break
+
+
+
+    else:
+        print("no number")
+
+
+
+
+
 
 
 
