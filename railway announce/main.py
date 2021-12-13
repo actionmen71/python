66b038f636b006337659a194b2a3a2ebf669fab9
actionmen71 <37360288+actionmen71@users.noreply.github.com>
2021-12-13 14:23:42 +0530

    Add files via upload

---
+++ railway announce/main.py
@@ -0,0 +1,35 @@
+import os
+import pandas as pd
+from pydub import AudioSegment
+from gtts import gTTS
+
+
+
+def text_to_speech(text, filename):
+    # this function will take text as input and convert it to mp3 speech file and give that file
+    # as output
+    pass
+
+def mergeAudios(audios):
+    pass
+
+def generate_Skeleton():
+    pass
+
+def generate_Announcement():
+    pass
+
+
+
+
+
+
+
+
+if __name__ == "__main__" :
+    print("Generating Skeleton...")
+    generate_Skeleton()
+    print("Now Generating Announcement...")
+    generate_Announcement()
+    
+
---
+++ railway announce/testing gtts.py
@@ -0,0 +1,2 @@
+import gtts
+gtts-cli 'hello' --output hello.mp3
